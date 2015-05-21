94febe9ee5a9944bfa3be1750dc8bdd7d5b91886
Brendan McGivern <bmcg0707@gmail.com>
2015-05-21 14:13:58 -0400

    assignment 27 completed, CRUD

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150520003049) do
+ActiveRecord::Schema.define(version: 20150521175150) do
+
+  create_table "advertisements", force: :cascade do |t|
+    t.string   "title"
+    t.text     "copy"
+    t.integer  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
