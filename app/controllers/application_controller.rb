4689db2ee9dcb794617374e22b62975b1d75a387
Brendan McGivern <bmcg0707@gmail.com>
2015-05-25 13:52:16 -0400

    assignment 31 completed, authentication continued

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
- 
+
+  before_action :flash_attack
+
+  def flash_attack
+    flash[:notice] = "This is a notice from flash_attack"
+  end
+
   protected
  
   def configure_permitted_parameters
