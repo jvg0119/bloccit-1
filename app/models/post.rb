96e9b7ec1713845ad91e9e5eea41fd57d5b122c8
Brendan McGivern <bmcg0707@gmail.com>
2015-05-25 18:11:32 -0400

    assignment 32 completed, associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,9 @@ class Post < ActiveRecord::Base
 
   # lists posts from newest to oldest
   default_scope { order('created_at DESC') }
+
+  # could have also done it this way
+  # scope :ordered_by_title, -> { reorder('posts.title ASC') }
+  scope :ordered_by_title, -> { reorder(title: 'ASC') }
+  scope :ordered_by_reverse_created_at, -> { reorder(created_at: 'DESC') }
 end
