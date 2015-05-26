ee210f76c0b8af144d7b8bd28fc518b52b991e49
Brendan McGivern <bmcg0707@gmail.com>
2015-05-26 19:25:57 -0400

    assignment 33 completed, authorization

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,5 +1,19 @@
 class PostPolicy < ApplicationPolicy
+  
   def index?
     true
   end
+
+  class Scope < Scope
+    def resolve
+      if !user.present?
+        scope.none
+      elsif user.admin? || user.moderator?
+        scope.all
+      else
+        scope.where(user: user)
+      end
+    end
+  end
+
 end
