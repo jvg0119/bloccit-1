ff4cee548fbf1fd0a94bbf1336911d90b9c9533d
Brendan McGivern <bmcg0707@gmail.com>
2015-05-27 08:45:40 -0400

    checkpoint 33 progress

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
+  belongs_to :topic
 
   # lists posts from newest to oldest
   default_scope { order('created_at DESC') }
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150526160500) do
+ActiveRecord::Schema.define(version: 20150527001416) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -36,14 +36,24 @@ ActiveRecord::Schema.define(version: 20150526160500) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.integer  "topic_id"
   end
 
+  add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.boolean  "resolved",   default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "public",      default: true
+    t.text     "description"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
   end
