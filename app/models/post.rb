529dde15d8a83988929a6da2ea74ee7626c54f1c
Brendan McGivern <bmcg0707@gmail.com>
2015-06-04 14:04:24 -0400

    assignment 49 complete, voting

--- app/models/post.rb
+++ app/models/post.rb
@@ -27,6 +27,8 @@ class Post < ActiveRecord::Base
   validates :topic, presence: true
   validates :user, presence: true
 
+  after_create :create_vote
+
   def up_votes
     votes.where(value: 1).count
   end
@@ -45,5 +47,11 @@ class Post < ActiveRecord::Base
 
     update_attribute(:rank, new_rank)
   end
+
+  private
+
+    def create_vote
+      user.votes.create(value: 1, post: self)
+    end
 
 end
